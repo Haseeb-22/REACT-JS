254c662a6fee024fd74414b3e37e0fe31aa44609
Haseeb-22 <112949997+Haseeb-22@users.noreply.github.com>
2022-09-27 17:37:53 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,71 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+
+
+  let [txt,setTxt] = useState();
+  let [list,setlist] = useState([]);
+   
+  function add(){
+    list.push(txt);
+    console.log(list)
+    setlist([...list]);
+  }
+
+  
+ const deleteall = (i) => {
+    const updateditems = list.filter((txt) => {
+        return i !== txt.value;
+    });
+
+    setlist(updateditems);
+
+  }
+
+ 
+  const onDelete = (txt) => {
+    console.log("I am ondelete of todo", txt);
+
+    setlist(list.filter((e) => {
+      return e !== txt;
+    }));
+    console.log("deleted", list)
+  }
+
+
+
+  
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <h3>Todo App</h3>
+
+        <input onChange={(e)=>{
+          setTxt(e.target.value);
+          console.log(txt)
+
+        }} />
+        <br />
+        <button onClick={()=>add()}>ADD</button> <br />
+        <button  onClick={() => deleteall(txt.value)}>Delete All</button>
+
+
+       {/* Print The Input */}
+        <ul>{list.map((e,i)=>{
+          return <li key={i}>{e} &nbsp; <button onClick={()=>onDelete(e)}>Delete</button> </li> })}
+        </ul>
+
+  
+      
+            
+      </header>
+    </div>
+  );
+}
+
+export default App;
